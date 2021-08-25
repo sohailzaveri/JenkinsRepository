9c5618daf72562596ce97d32cd92385f69fc94ff
Sohail Zaveri <sohail.zaveri@gmail.com>
2021-08-26 00:05:48 +0530

    Added a new line in HelloWorld2.java

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,6 +1,7 @@
 public class HelloWorld2{
 	public static void main(String args[]){
 		System.out.println("This is the second Hello World file. 
-		\n ****** Hello World 2 to Jenkins ********")
+		\n ****** Hello World 2 to Jenkins ********
+		\n Added a new line*********")
 	}
 }
