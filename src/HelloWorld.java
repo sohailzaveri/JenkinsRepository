09897b1df8dec137b778409007e62500325bc7a9
Sohail Zaveri <sohail.zaveri@gmail.com>
2021-08-26 00:29:30 +0530

    Testing trigger

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("************Hello Welcome to Jenkins I am Sohail****************");
+        System.out.println("************Hello Welcome to Jenkins I am Sohail****************
+		\n I added a new Line to trigger the build");
     }
 }
