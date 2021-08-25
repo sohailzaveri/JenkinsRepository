6e4ff37ab8481d63c18c9cc3d35dd8b60fe8737d
Sohail Zaveri <sohail.zaveri@gmail.com>
2021-08-25 23:59:58 +0530

    Commit on mybranch, created HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,6 @@
+public class HelloWorld2{
+	public static void main(String args[]){
+		System.out.println("This is the second Hello World file. 
+		\n ****** Hello World 2 to Jenkins ********")
+	}
+}
